c250c18abf27ab9867601cbabd99644bbe6384db
AnujanAsok <anujan.asokkumar@gmail.com>
2022-05-31 16:43:16 -0400

    adding prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { supabase } from './supabase_client';
+import logo from "./logo.svg";
+import "./App.css";
+import { supabase } from "./supabase_client";
 
 function App() {
   const getProfile = async () => {
-  let { data, error, status } = await supabase
-  .from('initial-test')
-  .select(`color`)
-  console.log(data)
-  }
-  getProfile()
+    let { data, error, status } = await supabase
+      .from("initial-test")
+      .select(`color`);
+    console.log(data);
+  };
+  getProfile();
   return (
     <div className="App">
       <header className="App-header">
